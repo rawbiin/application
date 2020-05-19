a785a5604b734c3aec649b92946ce5bcb2a37998
Dell <slowrock0000@gmail.com>
2020-05-19 21:41:23 +0545

    Project For Login And Signup.

---
+++ app/src/main/java/com/example/loginform/Login_form.java
@@ -0,0 +1,22 @@
+package com.example.loginform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+
+        getSupportActionBar().setTitle("Login Form");
+    }
+
+    public void btn_SignupForm(View view) {
+        startActivity(new Intent(getApplicationContext(),Signup_form.class));
+    }
+}
---
+++ app/src/main/java/com/example/loginform/Signup_form.java
@@ -0,0 +1,17 @@
+package com.example.loginform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_form);
+    getSupportActionBar().setTitle("Signup Form");
+
+    }
+
+}
